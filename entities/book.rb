f448d159deda3ee336cc7222dc32ee864545b801
GKirill <kir.gavrish@gmail.com>
2023-03-04 16:42:47 +0200

    add author validation for book

--- entities/book.rb
+++ entities/book.rb
@@ -1,7 +1,14 @@
+require "/Users/kirillgavris/Desktop/Library/extentions/validations.rb"
+require "/Users/kirillgavris/Desktop/Library/entities/author.rb"
+
 class Book
+    include Validations
+
     attr_reader :title, :author
 
     def initialize(title, author)
+        check_instance("Author", author, ::Author)
+
         @title = title
         @author = author
     end

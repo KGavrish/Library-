0ae0c5cf9465921a66f0635446a89271b5c3d111
GKirill <kir.gavrish@gmail.com>
2023-03-17 12:04:32 +0200

    add reader, book validation and date

--- entities/order.rb
+++ entities/order.rb
@@ -1,14 +1,21 @@
+require "/Users/kirillgavris/Desktop/Library/extentions/validations.rb"
+require "/Users/kirillgavris/Desktop/Library/entities/book.rb"
+require "/Users/kirillgavris/Desktop/Library/entities/reader.rb"
+
 class Order
+    include Validations
+
     attr_accessor :book, :reader, :date
 
-    def initialize(book, reader, date)
+    def initialize(book, reader, date = Time.now)
+        check_instance("Book", book, ::Book)
+        check_instance("Reader",reader, ::Reader)
         @book = book
         @reader = reader
         @date = date
     end
 
     def to_s
-        "#{book} was bought by #{reader} at #{date}"
+        "#{book} was bought by #{reader} at #{date.strftime("%d/%m/%Y %H:%M")}" 
     end
-
 end
